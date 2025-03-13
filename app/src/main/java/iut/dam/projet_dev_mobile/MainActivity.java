89b65c7f8b22bfc5fa057ccd67df5905b42173c6
FlorianKP <kopflorian@gmail.com>
2025-03-13 09:45:37 +0100

    main activity et connexion bd

--- app/src/main/java/iut/dam/projet_dev_mobile/MainActivity.java
+++ app/src/main/java/iut/dam/projet_dev_mobile/MainActivity.java
@@ -1,10 +1,12 @@
 package iut.dam.projet_dev_mobile;
 
 import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
 public class MainActivity extends AppCompatActivity {
 
+    String TAG = "server";
+    String urlString = "http://[192.168.238.219]/powerhome_server/getHabitats.php";
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        getRemoteHabitats();
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         if(bundle!=null) {
@@ -36,4 +43,30 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    ProgressDialog pDialog;
+    public void getRemoteHabitats() {
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Getting list of habitats...");
+        pDialog.setIndeterminate(true);
+        pDialog.setCancelable(false);
+        pDialog.show();
+        String urlString = "http://[server]/powerhome_server/getHabitats.php";
+        Ion.with(this)
+                .load(urlString)
+                .asString()
+                .setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        pDialog.dismiss();
+                        if (result == null)
+                            Log.d(TAG, "No response from the server!!!");
+                        else {
+                            Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    // Traitement de result
+                });
+    }
+
 }
