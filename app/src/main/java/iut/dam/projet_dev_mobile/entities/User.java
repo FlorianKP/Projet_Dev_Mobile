6dba5392925bba09aeee7f072724573298bbc3d2
FlorianKP <kopflorian@gmail.com>
2025-03-25 22:55:36 +0100

    noms des résidents dans la liste des habitats

--- app/src/main/java/iut/dam/projet_dev_mobile/entities/User.java
+++ app/src/main/java/iut/dam/projet_dev_mobile/entities/User.java
@@ -1,17 +1,20 @@
 package iut.dam.projet_dev_mobile.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.Date;
 
 public class User {
 
     public int id;
+    @SerializedName("firstname")
     public String firstName;
+    @SerializedName("lastname")
     public String lastName;
     public String email;
     public String password;
     public String token;
     public Date expiredAt;
-    public Habitat habitat;
 
     public User() {
     }
